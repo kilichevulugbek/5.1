29c9a7d934e7176a99738ce0d4aeae47d31e8795
kilichevulugbek <ulugbekkilichev0000@gmail.com>
2025-08-28 10:58:41 +0500

    Add files via upload

---
+++ lesson.js
@@ -0,0 +1,12 @@
+let ism = prompt("Ismingizni kiriting")
+let surname = prompt("Familyangizni kiriting")
+let age = prompt("Yoshingizni kiriting")
+let fan = prompt("Sevimli faningizni kiriting")
+let sport = prompt("Sevimli sport turini kiriting")
+alert("Malumotlaringiz qabul qilindi")
+
+console.log(`Ism: ${ism}`);
+console.log(`Familya: ${surname}`);
+console.log(`Yosh: ${age}`);
+console.log(`Sevimli fan: ${fan}`);
+console.log(`Sevimli sport turi: ${sport}`);
